90ac376b3b526e74fc0680686ff7b710cbfbb82f
htgurukul <himanshu.gurukul@gmail.com>
2019-04-10 08:59:28 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
                                         
                                         echo "Inside Dir component"
                                         echo "Treating Workspaces as $WORKSPACE"
-                                        sh """  env ; cat README.md """
+                                        sh """ pwd; cat README.md """
                                                 
 
                                 }
