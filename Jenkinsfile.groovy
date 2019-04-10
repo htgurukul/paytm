39c9e3ced02573282e245b392d3cab47755dbd7d
htgurukul <himanshu.gurukul@gmail.com>
2019-04-10 16:44:15 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline {
 		      
 		      stage('load another'){
 			      steps {
-				      script { load bakar.groovy }
+				      script { load "bakar.groovy" }
 			      }
 		      }
                   
