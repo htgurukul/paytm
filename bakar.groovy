455392204889ee5010043d9e4a6dfa8a2166afbb
htgurukul <himanshu.gurukul@gmail.com>
2019-04-10 08:57:25 +0530

    Add files via upload

---
+++ bakar.groovy
@@ -0,0 +1,20 @@
+pipeline {
+                agent any
+            
+              stages {
+                        
+
+                        stage('Future Now') {
+                                steps {
+                                        
+                                        echo "Future component"
+                                        
+                                                
+
+                                }
+                                
+
+                                }
+                  
+                }
+}
